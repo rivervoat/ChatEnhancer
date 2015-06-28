6665dce299637c5180213f695d1dfe8a49da4cdd
rivervoat <rivervoat@gmx.com>
2015-06-28 13:32:46 -0400

    use eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-eslint'),
+    concat = require('gulp-concat'),
+    babel = require('gulp-babel');
 
 var main = function() {
-    var gulp = require('gulp'),
-        jshint = require('gulp-jshint'),
-        concat = require('gulp-concat'),
-        babel = require('gulp-babel');
 
+    gulp.task('lint', function(){
+        return gulp.src(['src/**/*.js'])
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failOnError());        
+        
+    });
     gulp.task('build-js', function(){
         return gulp.src('src/ChatEnhancer.js')
             .pipe(babel())
@@ -19,6 +26,7 @@ var main = function() {
         
     });
     gulp.task('build-all', ['lint','build-js', 'build-chrome-ext']);
+    gulp.task('default', ['build-all']);
 };
 
 main();
