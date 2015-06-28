57bb7b86c05d6f54f8c0b224ac78edd1139b81c5
rivervoat <rivervoat@gmx.com>
2015-06-28 14:10:47 -0400

    gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,30 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-eslint'),
-    concat = require('gulp-concat'),
-    babel = require('gulp-babel');
+   eslint = require('gulp-eslint'),
+   concat = require('gulp-concat'),
+   babel = require('gulp-babel');
 
 var main = function() {
-
+    
     gulp.task('lint', function(){
         return gulp.src(['src/**/*.js'])
             .pipe(eslint())
             .pipe(eslint.format())
-            .pipe(eslint.failOnError());        
-        
+            .pipe(eslint.failOnError());
+    });    
+    gulp.task('clean-all', function(cb){
+        gulp.src('target/*').pipe(rm());
+        cb();
     });
-    gulp.task('build-js', function(){
+    gulp.task('build-js', ['lint', 'clean-all'], function(){
         return gulp.src('src/ChatEnhancer.js')
             .pipe(babel())
-            .pipe(gulp.dest('build'));
+            .pipe(gulp.dest('target/lib/'));
     });
-    gulp.task('build-chrome-ext', function(){
-        
+    gulp.task('build-chrome-ext', ['build-js'], function(){
     });
-    gulp.task('build-cfx-ff', function(){
+    gulp.task('build-cfx-ff', ['build-js'], function(){
     });
-    gulp.task('clean-all',function(){
-        
-    });
-    gulp.task('build-all', ['lint','build-js', 'build-chrome-ext']);
+    gulp.task('build-all', ['build-chrome-ext']);
     gulp.task('default', ['build-all']);
 };
 
