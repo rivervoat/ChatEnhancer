a84418f7152771074b96f17c2aca066d36220f26
rivervoat <rivervoat@gmx.com>
2015-06-28 16:36:12 -0400

    switch to gulp-browserify for a more concise and less error-prone gulp build, move babelify transform to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
+
+//hide eslint warnings about require
+var require = (require !== undefined) ? require : function(){};
+
 var gulp = require('gulp'),
-   eslint = require('gulp-eslint'),
-   concat = require('gulp-concat'),
-   babel = require('gulp-babel');
+    eslint = require('gulp-eslint'),
+    browserify = require('gulp-browserify'),
+    del = require('del');
 
 var main = function() {
-    
+
     gulp.task('lint', function(){
-        return gulp.src(['src/**/*.js'])
+        return gulp.src(['src/eg/**/*.js'])
             .pipe(eslint())
             .pipe(eslint.format())
             .pipe(eslint.failOnError());
-    });    
+    });
     gulp.task('clean-all', function(cb){
-        gulp.src('target/*').pipe(rm());
-        cb();
+        del(['target/*'], cb);
     });
     gulp.task('build-js', ['lint', 'clean-all'], function(){
+        //this task snippet found on blog of A. Coard
+        //http://advantcomp.com/blog/ES6Modules/
         return gulp.src('src/ChatEnhancer.js')
-            .pipe(babel())
+            .pipe(browserify({
+                standalone: 'beep'
+            }))
             .pipe(gulp.dest('target/lib/'));
     });
     gulp.task('build-chrome-ext', ['build-js'], function(){
